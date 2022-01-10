2880851d0256a5a892438f576224ea3b1e70f0e2
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2022-01-10 18:52:10 +0530

    Create calendar.py

---
+++ calendar.py
@@ -0,0 +1,7 @@
+# // Type "help", "copyright", "credits" or "license" for more information.
+# type python in cmd to call 
+
+import calendar 
+cal = calendar.month(2021,9) #year, month
+print(cal)
+   
